a6f7fe0da41085ddcbe443ca73f2f3365829c8d0
enockabere <maebaenock95@gmail>
2021-07-10 15:37:32 +0300

    add testimonial

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -233,3 +233,19 @@ $(document).ready(function () {
         $("#jumbs").hide(1000);
     });
 });
+//testimonial
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:true,
+        navigation:false,
+        navigationText:["",""],
+        slideSpeed:1000,
+        singleItem:true,
+        transitionStyle:"fade",
+        autoPlay:true
+    });
+});
